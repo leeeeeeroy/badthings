6ea9c936e5dd1654c231123de9d5ace7e37f70bd
Leroy <>
2020-11-16 15:09:31 -0500

    Added react-menu.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4c18c0f92543948e5b28";
+/******/ 	var hotCurrentHash = "aab6b9502efaf7b9bb7c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
